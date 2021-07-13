cc485849c8293b1e1f45040ce67b819ec2888faf
shedy-pub <sherleen6656@gmail.com>
2021-07-13 11:58:37 +0800

    update

--- script/train_jittor_aadb.sh
+++ script/train_jittor_aadb.sh
@@ -5,7 +5,7 @@ ARCH='resnet50_HLAGCN'
 VERSION="v0"
 DATASET="aadb"
 CUDA_VISIBLE_DEVICES=0,1,2,3 mpirun -np 4 python -m utils_jittor.train_jittor --arch ${ARCH} \
-        --epochs 20 -b 16 --lr 1e-2 --period 4 \
+        --epochs 20 -b 100 --lr 1e-2 --period 4 \
         --dataset ${DATASET} --dataroot "/home/xxx/data/AADB_dataset/" \
         --weight_dir "results/${ARCH}_${VERSION}_${DATASET}" 2>&1 | tee -a "logs/${ARCH}_${VERSION}_${DATASET}.txt"
      
